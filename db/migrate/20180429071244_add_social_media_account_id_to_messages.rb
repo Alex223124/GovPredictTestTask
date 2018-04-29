ce13aae06701d501a68ef7519b2b6ca54a8cf0ad
Al <sologoubalex@gmail.com>
2018-04-29 11:04:22 +0300

    filed fixed (should be social_media_account_id)

--- db/migrate/20180429071244_add_social_media_account_id_to_messages.rb
+++ db/migrate/20180429071244_add_social_media_account_id_to_messages.rb
@@ -1,5 +1,5 @@
 class AddSocialMediaAccountIdToMessages < ActiveRecord::Migration
   def change
-    add_column :messages, :social_media_account, :integer
+    add_column :messages, :social_media_account_id, :integer
   end
 end
