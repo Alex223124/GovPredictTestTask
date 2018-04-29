772c4035f12ef9802c5575267adfd539cda3eb3b
Al <sologoubalex@gmail.com>
2018-04-29 12:07:16 +0300

    association added: Person has_many messages and Message has_one person

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ActiveRecord::Base
 
+  has_one :person, through: :social_media_account, :source => "sociable", source_type: "Person"
   belongs_to :social_media_account
 
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -2,5 +2,6 @@ class Person < ActiveRecord::Base
 
   belongs_to :custom_list
   has_many :social_media_accounts, as: :sociable
+  has_many :messages, through: :social_media_accounts
 
 end
