5c359c07606c1347cd8895158e26b256f480d100
Al <sologoubalex@gmail.com>
2018-04-30 09:24:07 +0300

    specs for MessagesFilterForm#filter_by_social_media added

--- spec/forms/messages_filter_form_spec.rb
+++ spec/forms/messages_filter_form_spec.rb
@@ -159,5 +159,69 @@ describe MessagesFilterForm do
 
   end
 
+  context "#filter_by_social_media" do
+
+    context "when correct types are present" do
+
+      before(:each) do
+        @first = create(:message, :with_twitter_soc_media_account)
+        @second = create(:message, :with_facebook_soc_media_account)
+        @third = create(:message)
+        @fourth = create(:message)
+        @fifth = create(:message)
+      end
+
+      let(:expected_result) { [@first, @second] }
+
+      it "should filter messages by date range" do
+        filter_form = build(:messages_filter_form, :with_allowed_social_media_types)
+        filter_form.instance_variable_set("@relation", Message.all)
+        filter_form.send(:filter_by_social_media)
+        expect(filter_form.instance_variable_get("@relation")).to match_array(expected_result)
+      end
+
+    end
+
+    context "when types are not present" do
+
+      before(:each) do
+        @first = create(:message, :posted_one_month_ago)
+        @second = create(:message, :posted_one_month_ago)
+        @third = create(:message, :posted_two_months_ago)
+        @fourth = create(:message, :posted_two_months_ago)
+      end
+
+      let(:expected_result) { [@first, @second, @third, @fourth] }
+
+      it "shouldn't change amount of objects in @raleation" do
+        filter_form = build(:messages_filter_form)
+        filter_form.instance_variable_set("@relation", Message.all)
+        filter_form.send(:filter_by_social_media)
+        expect(filter_form.instance_variable_get("@relation")).to match_array(expected_result)
+      end
+
+    end
+
+    context "when form has incorrect values in @social_media_lists" do
+
+      before(:each) do
+        @first = create(:message, :posted_one_month_ago)
+        @second = create(:message, :posted_one_month_ago)
+        @third = create(:message, :posted_two_months_ago)
+      end
+
+      let(:expected_result) { [@first, @second, @third] }
+
+      it "shouldn't change amount of objects in @raleation" do
+        filter_form = build(:messages_filter_form, :with_incorrect_social_media_types)
+        filter_form.instance_variable_set("@relation", Message.all)
+        filter_form.send(:filter_by_social_media)
+        expect(filter_form.instance_variable_get("@relation")).to match_array(expected_result)
+      end
+
+    end
+
+  end
+  
 end
 
